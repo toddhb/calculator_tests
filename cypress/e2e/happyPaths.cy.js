bc4ab6c38b3585760c658eb0178572ab9895e521
BROCHU_HOME_PC\broch <brochut2@gmail.com>
2024-05-14 16:19:51 -0700

    Add tests

---
+++ cypress/e2e/happyPaths.cy.js
@@ -0,0 +1,80 @@
+const calculator = require('../support/functions/calculator.js')
+
+beforeEach(() => {
+    calculator.goTo()
+})
+
+const expectedLastResult = "98.7 + 6 - 5 × 432 ÷ 1.01 =",
+expectedInputFieldValue = "-2033.91386139"
+
+describe('happy paths', () => {
+  Cypress.on('uncaught:exception', () => false)
+
+  it('completes a calculation by button presses', () => {
+    // calculate by button presses: 98.7 + 6 - 5 × 432 ÷ 1.01 = -2033.9138613861385
+    calculator.getNumberKey("9").click()
+    calculator.getNumberKey("8").click()
+    calculator.getOperatorKey("point").click()
+    calculator.getNumberKey("7").click()
+    calculator.getOperatorKey("plus").click()
+    calculator.getNumberKey("6").click()
+    calculator.getOperatorKey("minus").click()
+    calculator.getNumberKey("5").click()
+    calculator.getOperatorKey("multiply").click()
+    calculator.getNumberKey("4").click()
+    calculator.getNumberKey("3").click()
+    calculator.getNumberKey("2").click()
+    calculator.getOperatorKey("divide").click()
+    calculator.getNumberKey("1").click()
+    calculator.getOperatorKey("point").click()
+    calculator.getNumberKey("0").click()
+    calculator.getNumberKey("1").click()
+
+    // the last result is 0 since this is a fresh calculation
+    expect(calculator.getLastResult().should("have.text", "Ans = 0"))
+
+    calculator.getOperatorKey("equals").click()
+
+    expect(calculator.getLastResult().should("have.text", expectedLastResult))
+    expect(calculator.getInputField().should("have.text", expectedInputFieldValue))
+  })
+
+  it('completes a calculation by keypad entry', () => {
+    // calculate by keypad entry: 98.7 + 6 - 5 * 432 / 1.01 = -2033.9138613861385
+    calculator.getInputField().type("98.7+6-5*432/1.01=")
+
+    expect(calculator.getLastResult().should("have.text", expectedLastResult))
+    expect(calculator.getInputField().should("have.text", expectedInputFieldValue))
+
+    // redo the calculation, but this time finish with the Enter key
+    calculator.getOperatorKey("all clear").click()
+    calculator.getInputField().type(`98.7+6-5*432/1.01{enter}`)
+
+    expect(calculator.getLastResult().should("have.text", expectedLastResult))
+    expect(calculator.getInputField().should("have.text", expectedInputFieldValue))
+  })
+
+  it('clears the input field using the AC button', () => {
+    // clear by using the AC button
+    calculator.getNumberKey("1").click()
+    calculator.getNumberKey("2").click()
+    calculator.getNumberKey("3").click()
+    calculator.getNumberKey("4").click()
+    calculator.getOperatorKey("clear entry").click()
+
+    expect(calculator.getLastResult().should("have.text", "Ans = 0"))
+    expect(calculator.getInputField().should("have.text", "123"))
+
+    // clear by Backspace button presses
+    calculator.getInputField().type(`{backspace}`)
+
+    expect(calculator.getLastResult().should("have.text", "Ans = 0"))
+    expect(calculator.getInputField().should("have.text", "12"))
+
+    // add back a valid digit
+    calculator.getInputField().type("5")
+
+    expect(calculator.getLastResult().should("have.text", "Ans = 0"))
+    expect(calculator.getInputField().should("have.text", "125"))
+  })
+})
--- cypress/e2e/styling.cy.js
+++ cypress/e2e/styling.cy.js
@@ -9,6 +9,7 @@ before(() => {
 
 describe('styling verifications', () => {
   Cypress.on('uncaught:exception', () => false)
+
   it('styling verifications', () => {
     // defaults to 0 in the input field on page load
     expect(calculator.getInputField().should('have.text', '0'))
@@ -16,39 +17,39 @@ describe('styling verifications', () => {
     // displays all the operator keys
     expect(calculator.getOperatorKey("all clear").should('be.visible'))
     calculator.getOperatorKey('all clear').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     calculator.getOperatorKey('all clear').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     expect(calculator.getOperatorKey("divide").should('be.visible'))
     calculator.getOperatorKey('divide').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
-        calculator.getOperatorKey('divide').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
+    calculator.getOperatorKey('divide').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     expect(calculator.getOperatorKey("multiply").should('be.visible'))
     calculator.getOperatorKey('multiply').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
-        calculator.getOperatorKey('multiply').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
+    calculator.getOperatorKey('multiply').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     expect(calculator.getOperatorKey("minus").should('be.visible'))
     calculator.getOperatorKey('minus').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
-        calculator.getOperatorKey('minus').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
+    calculator.getOperatorKey('minus').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     expect(calculator.getOperatorKey("plus").should('be.visible'))
     calculator.getOperatorKey('plus').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
-        calculator.getOperatorKey('plus').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.gray)
-        })
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
+    calculator.getOperatorKey('plus').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.gray)
+    })
     expect(calculator.getOperatorKey("equals").should('be.visible'))
     calculator.getOperatorKey('equals').invoke('css', 'background-color').then((color) => {
       expect(rgbHex(color)).to.eq(colors.blue)
@@ -58,31 +59,31 @@ describe('styling verifications', () => {
     })
     expect(calculator.getOperatorKey("point").should('be.visible'))
     calculator.getOperatorKey('point').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
-        calculator.getOperatorKey('point').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
+    calculator.getOperatorKey('point').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
 
     // displays all the number keys
     expect(calculator.getNumberKey("0").should('be.visible'))
     calculator.getNumberKey('0').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
-        calculator.getNumberKey('0').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
+    calculator.getNumberKey('0').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
     expect(calculator.getNumberKey("1").should('be.visible'))
     calculator.getNumberKey('1').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
-        calculator.getNumberKey('1').invoke('css', 'border-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
+    calculator.getNumberKey('1').invoke('css', 'border-color').then((color) => {
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
     expect(calculator.getNumberKey("2").should('be.visible'))
     calculator.getNumberKey('2').invoke('css', 'background-color').then((color) => {
-          expect(rgbHex(color)).to.eq(colors.white)
-        })
+      expect(rgbHex(color)).to.eq(colors.white)
+    })
     calculator.getNumberKey('2').invoke('css', 'border-color').then((color) => {
       expect(rgbHex(color)).to.eq(colors.white)
     })
