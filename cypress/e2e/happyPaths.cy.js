a66ddf7d6b69a64b6da6fa2828ab5fdc8a953546
BROCHU_HOME_PC\broch <brochut2@gmail.com>
2024-05-14 18:09:37 -0700

    Move trig functions to happy path file

--- cypress/e2e/happyPaths.cy.js
+++ cypress/e2e/happyPaths.cy.js
@@ -77,4 +77,67 @@ describe('happy paths', () => {
     expect(calculator.getLastResult().should("have.text", "Ans = 0"))
     expect(calculator.getInputField().should("have.text", "125"))
   })
+
+  it('represents the last result with the letter input "a"', () => {
+    calculator.getInputField().type("789a+6=")
+
+    expect(calculator.getLastResult().should("have.text", "789 Ans + 6 ="))
+    expect(calculator.getInputField().should("have.text", "6"))
+
+    calculator.getInputField().type("456+7=a+8=")
+
+    expect(calculator.getLastResult().should("have.text", "Ans + 8 ="))
+    expect(calculator.getInputField().should("have.text", "471"))
+  })
+
+  it('represents a cosine function with the letter input "c"', () => {
+    calculator.getInputField().type("c321=")
+
+    expect(calculator.getLastResult().should("have.text", "cos(321) ="))
+    expect(calculator.getInputField().should("have.text", "0.84855432554"))
+  })
+
+  it('represents the base of the natural log with the letter input "e"', () => {
+    // Lowercase e is the base of the natural logarithm, equal to approximately 2.78.
+    calculator.getInputField().type("e*e=")
+
+    expect(calculator.getLastResult().should("have.text", "e × e ="))
+    expect(calculator.getInputField().should("have.text", "7.38905609893"))
+  })
+
+  it('represents a logarithm function with the letter input "g"', () => {
+    calculator.getInputField().type("157g8=")
+
+    expect(calculator.getLastResult().should("have.text", "157 log(8) ="))
+    expect(calculator.getInputField().should("have.text", "141.785127958"))
+
+    calculator.getOperatorKey("AC").click()
+    calculator.getInputField().type("157g=")
+
+    expect(calculator.getLastResult().should("have.text", "157 log() ="))
+    expect(calculator.getInputField().should("have.text", "Error"))
+  })
+
+  it('represents a natural logarithm function with the letter input "l"', () => {
+    calculator.getInputField().type("157l8=")
+
+    expect(calculator.getLastResult().should("have.text", "157 ln(8) ="))
+    expect(calculator.getInputField().should("have.text", "326.472322044"))
+
+    calculator.getOperatorKey("AC").click()
+    calculator.getInputField().type("157l=")
+
+    expect(calculator.getLastResult().should("have.text", "157 ln() ="))
+    expect(calculator.getInputField().should("have.text", "Error"))
+  })
+
+  it('represents an arc cosine function with the letter input "C"', () => {
+    calculator.getInputField().type("C4")
+    calculator.getOperatorKey("÷").click()
+    calculator.getNumberKey("5").click()
+    calculator.getOperatorKey("=").click()
+
+    expect(calculator.getLastResult().should("have.text", "arccos(4 ÷ 5) ="))
+    expect(calculator.getInputField().should("have.text", "0.64350110879"))
+  })
 })
